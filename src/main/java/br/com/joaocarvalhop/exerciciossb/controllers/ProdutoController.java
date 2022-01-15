a050f7f079f4f90944f58830e359505d09500f8c
joaocarvalhop <86267920+joaocarvalhop@users.noreply.github.com>
2022-01-15 15:38:39 -0300

    Simplificando inserir produto

--- src/main/java/br/com/joaocarvalhop/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/joaocarvalhop/exerciciossb/controllers/ProdutoController.java
@@ -1,7 +1,6 @@
 package br.com.joaocarvalhop.exerciciossb.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,11 +16,9 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
-	@PostMapping
-	public @ResponseBody Produto novoProduto(@RequestParam String nome, @RequestParam double preco,
-			@RequestParam double desconto) {
-		Produto produto = new Produto(nome, preco, desconto);
+	public @ResponseBody Produto novoProduto(@RequestParam Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
 	}
+
 }
