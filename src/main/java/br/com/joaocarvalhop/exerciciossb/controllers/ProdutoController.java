3470c2a762603f134834d6516dd0a1d288fde87c
joaocarvalhop <86267920+joaocarvalhop@users.noreply.github.com>
2022-01-18 16:17:07 -0300

    Consulta Paginada

--- src/main/java/br/com/joaocarvalhop/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/joaocarvalhop/exerciciossb/controllers/ProdutoController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,6 +53,12 @@ public class ProdutoController {
 	@DeleteMapping(path = "/{id}")
 	public void excluirProduto(@PathVariable int id) {
 		produtoRepository.deleteById(id);
+	}
+
+	@GetMapping(path = "/pagina/{numPagina}")
+	public Iterable<Produto> obterPorPagina(@PathVariable int numPagina) {
+		Pageable page = PageRequest.of(numPagina, 3);
+		return produtoRepository.findAll(page);
 	}
 
 }
--- src/main/java/br/com/joaocarvalhop/exerciciossb/model/repositories/ProdutoRepository.java
+++ src/main/java/br/com/joaocarvalhop/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,11 +1,12 @@
 package br.com.joaocarvalhop.exerciciossb.model.repositories;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.joaocarvalhop.exerciciossb.model.entities.Produto;
 
 // Essa classe é responsável por abstrair os dados
 // <primeiro defina a classe a ser trabalhada, tipo do id(Não acita tipos primitivos)>
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
 
 }
